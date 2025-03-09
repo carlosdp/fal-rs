b04423c8b118476accb42e85531fb1b2c99e32dc
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2025-03-08 20:21:47 -0500

    add queue status test

--- fal/tests/queue.rs
+++ fal/tests/queue.rs
@@ -1,4 +1,5 @@
 use fal::{queue::Status, FalMultiImageResponse};
+use futures::StreamExt;
 
 #[fal::endpoint(endpoint = "fal-ai/flux/dev")]
 fn fal_dev(prompt: String) -> FalMultiImageResponse {}
@@ -15,6 +16,23 @@ async fn test_queue() {
         }
 
         tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
+
+    let response = queue.response().await.unwrap();
+
+    assert!(response.images.len() > 0)
+}
+
+#[tokio::test]
+async fn test_queue_status_stream() {
+    let queue = fal_dev("a horse".to_owned()).queue().await.unwrap();
+
+    while let Some(status) = queue.stream().await.unwrap().next().await {
+        let status = status.unwrap();
+
+        if status.status == Status::Completed {
+            break;
+        }
     }
 
     let response = queue.response().await.unwrap();
